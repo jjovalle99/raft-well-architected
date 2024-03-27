bce627691d6015aca67ada3c2aa54592d2c54a48
jjovalle99 <jj.ovalle@uniandes.edu.co>
2024-03-26 23:01:34 -0500

    creating pydantic settings

---
+++ src/settings.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+
+class EnvironmentSettings(BaseSettings):
+    LANGCHAIN_TRACING_V2: str
+    LANGCHAIN_PROJECT: str
+    LANGCHAIN_API_KEY: str
+    OPENAI_API_KEY: str
+    ANTHROPIC_API_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+
+class Settings(BaseSettings):
+    env: EnvironmentSettings = EnvironmentSettings()
+
+
+settings = Settings()
